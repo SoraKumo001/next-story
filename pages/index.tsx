a6bbfbd096f59fd1c7288535ee6c1a023336e0c2
SoraKumo <info@croud.jp>
2020-02-15 22:25:29 +0900

    add-button

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,10 @@ import Link from "next/link";
 export default () => (
   <>
     <div>こんにちは世界！</div>
-    <Link href="page02"><a>Page2へ</a></Link>
+    <br />
+    <button>ボタンの追加</button>
+    <Link href="page02">
+      <a>Page2へ</a>
+    </Link>
   </>
 );
