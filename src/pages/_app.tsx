c7a9827a9318b95ffb12e12297c7cba416aed62f
Akihiko Oikawa <akihiko.oikawa@finc.com>
2020-02-17 18:56:41 +0900

    no message

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,33 @@
-import React from "react";
-import { Container } from "next/app";
-import { ApolloClient,HttpLink,InMemoryCache, NormalizedCacheObject } from "apollo-boost";
+import React, { Component } from "react";
+import App, { Container, AppContext, AppInitialProps } from "next/app";
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject
+} from "apollo-boost";
 import fetch from "isomorphic-unfetch";
 import { ApolloProvider } from "react-apollo";
+import session from "express-session";
+
+const s = session({
+  secret: "nextjs",
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: true, httpOnly: true, maxAge: 1000 * 60 * 30 }
+});
 
 const IS_BROWSER = !!process.browser;
 const URI_ENDPOINT = "https://api.github.com/graphql";
-const TOKEN = '';
-function createClient(initialState?:NormalizedCacheObject) {
+const TOKEN = "";
+function createClient(initialState?: NormalizedCacheObject) {
   return new ApolloClient({
     connectToDevTools: IS_BROWSER,
     ssrMode: !IS_BROWSER,
     link: new HttpLink({
       fetch: IS_BROWSER ? fetch : undefined,
       uri: URI_ENDPOINT,
-      headers:{Authorization: `bearer ${TOKEN}`},
+      headers: { Authorization: `bearer ${TOKEN}` },
       credentials: "same-origin"
     }),
     cache: new InMemoryCache().restore(initialState || {})
@@ -23,13 +36,36 @@ function createClient(initialState?:NormalizedCacheObject) {
 
 const client = createClient();
 
-export default props => {
-  const { Component, pageProps, apolloClient } = props;
-  return (
-    <Container>
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
-    </Container>
-  );
-};
+export default class _App extends App {
+  static getInitialProps({ Component, ctx }: AppContext) {
+    return new Promise<AppInitialProps>(resolv => {
+      if (ctx.req)
+        s(ctx.req as any, ctx.res as any, () => {
+          console.log(ctx.req["session"]);
+          ctx.req["session"]["test"] = new Date().toLocaleString();
+          // s(ctx.req as any, ctx.res as any,()=>{});
+          resolv(
+            (Component.getInitialProps
+              ? Component.getInitialProps(ctx)
+              : {}) as AppInitialProps
+          );
+        });
+      else
+        resolv(
+          (Component.getInitialProps
+            ? Component.getInitialProps(ctx)
+            : {}) as AppInitialProps
+        );
+    });
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </Container>
+    );
+  }
+}
